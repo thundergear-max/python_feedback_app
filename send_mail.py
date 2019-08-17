9f5b2722cd2fcf2df7fefc55d720f34619535416
thundergear-max <52620272+thundergear-max@users.noreply.github.com>
2019-08-17 14:37:11 +0530

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -5,8 +5,8 @@ from email.mime.text import MIMEText
 def send_mail(customer, dealer, rating, comments):
     port = 2525
     smtp_server = 'smtp.mailtrap.io'
-    login = ''
-    password = ''
+    login = '34a6735730d1f0'
+    password = '150a15c41e0e14'
     message = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>"
 
     sender_email = 'email1@example.com'
